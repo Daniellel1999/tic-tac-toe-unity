b0ba3131e95e2f31b27d6ffbeebb20343a5294e7
Daniellel1999 <chailelnekave@gmail.com>
2025-08-31 17:55:46 +0300

    feat: update markedSpaces initialization and enhance win condition check in Tic Tac Toe

--- Tic-Tac-Toe/Assets/Scripts/GameController.cs
+++ Tic-Tac-Toe/Assets/Scripts/GameController.cs
@@ -36,7 +36,7 @@ public class GameController : MonoBehaviour
         }
         for (int i = 0; i < markedSpaces.Length; i++)
         {
-            markedSpaces[i] = -1;
+            markedSpaces[i] = -100;
         }
     }
 
@@ -44,16 +44,19 @@ public class GameController : MonoBehaviour
     void Update()
     {
 
-
     }
     public void TicTacToeButton(int whichNumber)
     {
         tictactoeSpaces[whichNumber].image.sprite = playIcons[whoturn];
         tictactoeSpaces[whichNumber].interactable = false;
 
-        markedSpaces[whichNumber] = whoturn;
+        markedSpaces[whichNumber] = whoturn + 1;
         turnCount++;
 
+        if (turnCount >= 5)
+        {
+            CheckWinCondition();
+        }
         if (whoturn == 0)
         {
             whoturn = 1;
@@ -67,6 +70,31 @@ public class GameController : MonoBehaviour
             turnIcons[1].SetActive(false);
         }
 
-    }        
+    }
+    void CheckWinCondition()
+    {
+        int s1 = markedSpaces[0] + markedSpaces[1] + markedSpaces[2];
+        int s2 = markedSpaces[3] + markedSpaces[4] + markedSpaces[5];
+        int s3 = markedSpaces[6] + markedSpaces[7] + markedSpaces[8];
+        int s4 = markedSpaces[0] + markedSpaces[3] + markedSpaces[6];
+        int s5 = markedSpaces[1] + markedSpaces[4] + markedSpaces[7];
+        int s6 = markedSpaces[2] + markedSpaces[5] + markedSpaces[8];
+        int s7 = markedSpaces[0] + markedSpaces[4] + markedSpaces[8];
+        int s8 = markedSpaces[2] + markedSpaces[4] + markedSpaces[6];
+        var solutions = new int[] { s1, s2, s3, s4, s5, s6, s7, s8 };
+        for (int i = 0; i < solutions.Length; i++)
+        {
+            if (solutions[i]== 3*(whoturn+1))
+            {
+                Debug.Log("Player " + (whoturn + 1) + " wins!");
+                return;
+            }
+            else if (turnCount >= 9)
+            {
+                Debug.Log("It's a draw!");
+                return;
+            }
+        }
+    }      
     
 }
